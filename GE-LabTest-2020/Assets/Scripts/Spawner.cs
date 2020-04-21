5580bbacd63481b5a5525fb0ce04972c4567bf1f
CMorar143 <cian@morar.org>
2020-04-21 09:45:44 +0100

    procedurally find locations of traffic lights

---
+++ GE-LabTest-2020/Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public int numTrafficLights = 10;
+	public float radius = 10;
+	private List<Vector3> wayPoints = new List<Vector3>();
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+
+	public void OnDrawGizmos()
+	{
+		//CalculateWayPoints();
+		foreach (Vector3 p in wayPoints)
+		{
+			Gizmos.DrawWireSphere(p, 2);
+		}
+	}
+
+	// Use this for initialization
+	void Awake()
+	{
+		float thetaInc = (Mathf.PI * 2) / (float)numTrafficLights;
+		for (int i = 0; i < numTrafficLights; i++)
+		{
+			float theta = i * thetaInc;
+			Vector3 pos = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+			pos = transform.TransformPoint(pos);
+			wayPoints.Add(pos);
+		}
+	}
+}
